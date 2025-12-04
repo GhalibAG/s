24e7593a5ca75b6ef627495521bec850070c8b84
GhalibAG <ghalibgtx@gmail.com>
2025-12-04 09:31:57 +0700

    Aku aswin, abis ngoding java

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Deklarasi array dipindahkan ke sini agar bisa diakses oleh main
+        String cars[] = new String[4];
+        cars[0] = "Volvo";
+        cars[1] = "Toyota";
+        cars[2] = "Yamaha";
+        cars[3] = "Ford";
+        
+        // Perulangan dimulai dari 0 dan berakhir di 3 (total 4 iterasi)
+        // Indeks array di Java dimulai dari 0
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println("Mobil Ke- " + (i + 1) + " Adalah " + cars[i]);
+        }
+    }
+}
